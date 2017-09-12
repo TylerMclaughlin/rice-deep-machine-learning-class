9999212513dd6a2cc2f57761119c8b6dbca2333d
TylerMcLaughlin <rtylermclaughlin@gmail.com>
2017-09-12 18:36:36 -0500

    Make ELEC 576 Repository - deep learning nitial commit

---
+++ reading.checklist.py
@@ -0,0 +1,9 @@
+#1. conda cheat sheet. 
+#2. Numpy for MATLAB 
+#3. Version control
+#4. PyCharm Tutorial Videos
+#5. Nature Paper 2015, LeCun, Bengio, Hinton
+#6. Machine learning review - Gaussian Mixture Models
+#7. Factor Analysis
+8. IPython/Jupyter
+#9. Stealing Google's Coding Practices for Academia
